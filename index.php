cb7367d71b99607245f3ff91b0786d01137bac21
JP1005YT <godlolpro32@gmail.com>
2023-04-12 11:52:29 -0300

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use src\Aula;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    
+    $aula = new Aula();
+    $resposta = $aula -> buscarTiposAula();
+    echo $resposta;
+    return $response;
+});
+$app->setBasePath('/Back-End-Aula');
+$app->run();
---
+++ src/Aula.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace src;
+
+class Aula {
+    public function buscarTiposAula(){
+        
+        $local = 
+
+        // $variavel = file_get_contents('aula/aula.js');
+        // $variavel_js = json_decode(substr($variavel,20));
+        // $imports = $variavel_js->imports;
+        // $dados = [];
+        // foreach($imports as $local){
+        //     $return = file_get_contents('aula/'.$local);
+        //     $return_js = json_decode(rtrim(substr($return,14),")"));
+        //     $info = [
+        //         "id" => $return_js->id,
+        //         "tipo" => $return_js->tipo,
+        //     ];
+        //     $dados.array_push($dados,$info);
+        //     echo "
+        //     <div>
+        //         <strong>ID:</strong><span>$return_js->id</span> <->
+        //         <strong>Tipo:</strong><span>{{$return_js->tipo}}</span>
+        //     </div><hr>
+        //     ";
+        //     file_put_contents('aulas.json',json_encode($dados));
+        // }
+        
+    }
+}
